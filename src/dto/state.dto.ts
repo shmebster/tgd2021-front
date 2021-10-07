52c91145457332f61713379657f6bee21f29619f
Kirill Ivlev <ivlekir@hilti.com>
2021-10-08 00:57:08 +0300

    v0.2

--- src/dto/state.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface StateDto {
-    state: string;
-    value: string;
-}
--- src/views/Quiz.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <h1>Quiz</h1>
-</template>
-<script lang="ts">
-import { Vue } from "vue-class-component";
-
-export default class QuizComponent extends Vue {}
-</script>
